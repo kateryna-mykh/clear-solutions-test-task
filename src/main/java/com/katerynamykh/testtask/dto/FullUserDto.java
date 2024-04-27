178e46bdb9a4a9a828438e39e297aad3e1032d3c
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-04-27 04:51:47 +0300

    delete redundant

--- src/main/java/com/katerynamykh/testtask/dto/FullUserDto.java
+++ src/main/java/com/katerynamykh/testtask/dto/FullUserDto.java
@@ -1,6 +1,5 @@
 package com.katerynamykh.testtask.dto;
 
-import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 public record FullUserDto(
@@ -10,7 +9,6 @@ public record FullUserDto(
         String lastName,
         LocalDate birthDate,
         String address,
-        @Pattern(regexp = "\\+??\\d{12}|\\d{10}", message = "Phone number should be valid")
         String phoneNumber) {
 
 }
